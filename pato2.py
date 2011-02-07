a5872f3b259b97125089866169d06aa9a4d78e23
Joshua Ismael Haase Hernández <hahj87@gmail.com>
2011-02-07 12:30:30 -0600

    moved config out from pato2

--- pato2.py
+++ pato2.py
@@ -24,47 +24,12 @@
                                                                              
 
 """
+from repo_maintainer.config import *
 
 import tarfile, commands
 from glob import glob
 from user import home
 from os.path import isdir, isfile, realpath
-# ---------- Config Variables Start Here ---------- #
-
-time__ = commands.getoutput("date +%Y%m%d-%H:%M")
-
-# Mirror Parameters
-mirror = "mirrors.eu.kernel.org"
-mirrorpath = "::mirrors/archlinux"
-
-# Directories and files
-## Optionals
-path   = home + "/parabolagnulinux.org"
-docs   = path + "/docs"
-logdir = path + "/log"
-## Must be defined
-logname= logdir + "/" + time__ + "-repo-maintainer.log"
-repodir= path + "/repo"
-tmp    = home + "/tmp"
-archdb = tmp  + "/db"
-
-# Repo, arch, and other folders to use for repo
-repo_list = ("core", "extra", "community", "testing", "community-testing", "multilib")
-dir_list  = ("pool","sources")
-arch_list = ("i686", "x86_64")
-other     = ("any",)
-
-# Output
-output    = True
-verbose   = False
-
-# Files
-blacklist = docs + "/blacklist.txt"
-whitelist = docs + "/whitelist.txt"
-pending   = docs + "/pending"
-rsyncBlacklist = docs + "/rsyncBlacklist"
-
-# ---------- Config Variables End Here---------- #
 
 def printf(text,output_=output):
 	"""Guarda el texto en la variable log y puede imprimir en pantalla."""
