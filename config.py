317c0b412642b5268e8408f9d87e19261370cd32
Parabola <parabolavnx@lions.dreamhost.com>
2011-02-07 18:25:37 -0800

    fixed errors

---
+++ config.py
@@ -0,0 +1,41 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from user import home
+import commands
+
+time__ = commands.getoutput("date +%Y%m%d-%H:%M")
+
+# Mirror Parameters
+mirror = "mirrors.eu.kernel.org"
+mirrorpath = "::mirrors/archlinux"
+
+# Directories and files
+
+## Optionals
+path   = home + "/parabolagnulinux.org"
+docs   = path + "/docs"
+logdir = path + "/log"
+
+## Must be defined
+logname= logdir + "/" + time__ + "-repo-maintainer.log"
+repodir= path + "/repo"
+tmp    = home + "/tmp"
+archdb = tmp  + "/db"
+free_path= path + "/free/"
+
+# Repo, arch, and other folders to use for repo
+repo_list = ("core", "extra", "community", "testing", "community-testing", "multilib")
+dir_list  = ("pool","sources")
+arch_list = ("i686", "x86_64")
+other     = ("any",)
+
+# Output
+output    = True
+verbose   = False
+
+# Files
+blacklist = docs + "/blacklist.txt"
+whitelist = docs + "/whitelist.txt"
+pending   = docs + "/pending"
+rsyncBlacklist = docs + "/rsyncBlacklist"
