72d3e1102acf8e90a1646d5c9afa6fdd01b2aeca
Joshua Ismael Haase Hernández <hahj87@gmail.com>
2011-04-08 11:40:32 -0500

    Config ported to bash

---
+++ config.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+# Mirror options
+mirror="mirrors.eu.kernel.org"
+mirrorpath="::mirrors/archlinux"
+
+# Directories and files
+
+## Optionals
+paraboladir=~/parabolagnulinux.org
+logtime=$(date -u +%Y%m%d-%H:%M)
+
+## Must be defined
+logname=${paraboladir}/${logtime}-repo-maintainer.log
+tempdir=~/tmp/
+docs_dir=${paraboladir}/docs
+repodir=${paraboladir}/repo
+
+# Repos, arches, and dirs for repo
+repolist="core:extra:community:testing:community-testing:multilib"
+dir_list="pool"
+arch_list="i686:x86_64"
+other="any"
+
+# Output options
+output="True"
+debug="False"
+
+# Rsync commands
+rsync_list_command="rsync -a --no-motd --list-only "
+rsync_update_command="rsync -av --delay-updates --exclude=*.{abs|db}.tar.* "
+rsync_post_command="rsync -av --delete --exclude=*.abs.tar.* "
+
+
+function run_python_cmd {
+    env \
+	mirror=${mirror} \
+	mirrorpath=${mirrorpath} \
+	logname=${logname} \
+	tempdir=${tempdir} \
+	docs_dir=${docs_dir} \
+	repodir=${repodir} \
+	repolist=${repolist} \
+	dir_list=${dir_list} \
+	arch_list=${arch_list} \
+	other=${other} \
+	output=${output} \
+	debug=${debug} \
+	$1
+}
